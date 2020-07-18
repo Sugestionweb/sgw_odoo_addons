40dc53b1a466bbeb9babdd04da1b9864c5f841ea
javierobcn <javierobcn@gmail.com>
2020-07-18 20:35:43 +0200

    sgw_partner_history to save a note in the chatter whenever any field change.

--- sgw_partner_history/models/partner_inherited.py
+++ sgw_partner_history/models/partner_inherited.py
@@ -89,3 +89,7 @@ class SgwPartner(models.Model):
 #              "Use this field anywhere a small image is required.")
 #     # hack to allow using plain browse record in qweb views, and used in ir.qweb.field.contact
 #     self = fields.Many2one(comodel_name=_name, compute='_compute_get_ids')
+
+    property_payment_term_id = fields.Many2one(track_visibility = 'onchange')
+    customer_payment_mode_id = fields.Many2one(track_visibility = 'onchange')
+    
