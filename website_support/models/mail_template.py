bd98a063c9a70de3584202a3d167a68b67baa7be
javierobcn <javierobcn@gmail.com>
2020-12-24 12:11:07 +0100

    Add web_support

---
+++ website_support/models/mail_template.py
@@ -0,0 +1,11 @@
+from odoo import api, fields, models
+
+
+class MailTemplateSupportTicket(models.Model):
+
+    _inherit = "mail.template"
+
+    built_in = fields.Boolean(
+        string="Built in",
+        help="Seperates email templates created by modules and ones created by users",
+    )
